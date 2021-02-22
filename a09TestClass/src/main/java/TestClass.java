1884f24165333597391d41c68934002a2bcbe719
HymanLiuTS <879651072@qq.com>
2021-02-22 19:16:26 +0800

    添加辅助分析类的字节码文件的方法

---
+++ a09TestClass/src/main/java/TestClass.java
@@ -0,0 +1,15 @@
+/**
+ * @author ：Hyman
+ * @date ：Created in 2021/2/22 18:37
+ * @description：
+ * @modified By：
+ * @version: $
+ */
+public class TestClass {
+
+    private int m;
+
+    public int inc() {
+        return m + 1;
+    }
+}
