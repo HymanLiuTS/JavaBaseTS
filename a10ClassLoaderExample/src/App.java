60b3e681c61bdb40444b3f9ad6286dd5c06b3653
HymanLiuTS <879651072@qq.com>
2021-02-25 18:17:45 +0800

    提交类初始化的测试代码

--- a10ClassLoaderExample/src/App.java
+++ a10ClassLoaderExample/src/App.java
@@ -6,12 +6,24 @@
  * @version: $
  */
 public class App {
-    
+
     public static void main(String[] args) {
         /**
          *对于静态字段，只有直接定义了该字段的类才会初始化。通过子类
          * 引用父类的静态字段，只会触发父类的初始化，不会触发子类的初始化
          * */
         System.out.println(SubClass.value);
+
+        /**
+         * 数组的初始化不会触发类的初始化
+         *
+         * */
+        SuperClass[] sca=new SuperClass[10];
+
+        /**
+         * 使用类的const常量也不会导致类的初始化，因为再编译阶段，编译器
+         * 已经将常量放到了App类自己的常量池里面
+         * */
+        System.out.println(ConstClass2.HELLOWORLD);
     }
 }
---
+++ a10ClassLoaderExample/src/ConstClass2.java
@@ -0,0 +1,14 @@
+/**
+ * @author ：Hyman
+ * @date ：Created in 2021/2/25 18:09
+ * @description：
+ * @modified By：
+ * @version: $
+ */
+public class ConstClass2 {
+    static {
+        System.out.println("ConstClass  init!");
+    }
+
+    public static final String HELLOWORLD="Hello World";
+}
