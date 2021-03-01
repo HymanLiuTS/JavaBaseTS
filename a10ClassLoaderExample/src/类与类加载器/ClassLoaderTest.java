f20cc30b9eabc553ee68f500413896937815da51
HymanLiuTS <879651072@qq.com>
2021-03-01 11:47:26 +0800

    双亲委派模型

--- a10ClassLoaderExample/src/类与类加载器/ClassLoaderTest.java
+++ a10ClassLoaderExample/src/类与类加载器/ClassLoaderTest.java
@@ -1,5 +1,8 @@
 package 类与类加载器;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 /**
  * @author ：Hyman
  * @date ：Created in 2021/3/1 9:09
@@ -9,7 +12,29 @@ package 类与类加载器;
  */
 public class ClassLoaderTest {
 
-    public static void main(String[] arges) {
-        
+    public static void main(String[] arges) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+        ClassLoader myClassLoader = new ClassLoader() {
+
+            @Override
+            public Class<?> loadClass(String name) throws ClassNotFoundException {
+                try {
+                    String fieldName = name.substring(name.lastIndexOf(".") + 1) + ".class";
+                    InputStream is = getClass().getResourceAsStream(fieldName);
+                    if (is == null) {
+                        return super.loadClass(name);
+                    }
+                    byte[] b = new byte[is.available()];
+                    is.read(b);
+                    return defineClass(name, b, 0, b.length);
+                } catch (Exception e) {
+                    throw new ClassNotFoundException(name);
+                }
+
+            }
+        };
+
+        Object obj=myClassLoader.loadClass("类与类加载器.ClassLoaderTest").newInstance();
+        System.out.println(obj.getClass());
+        System.out.println(obj instanceof 类与类加载器.ClassLoaderTest);
     }
 }
