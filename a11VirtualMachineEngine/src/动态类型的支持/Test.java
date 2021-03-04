24c0ae193bbd989af110c55ceccd37a952b3d1db
HymanLiuTS <879651072@qq.com>
2021-03-04 11:43:25 +0800

    使用MethodHadler进行动态方法调用

--- a11VirtualMachineEngine/src/动态类型的支持/Test.java
+++ a11VirtualMachineEngine/src/动态类型的支持/Test.java
@@ -30,6 +30,8 @@ public class Test {
         @Override
         void thinking() throws Throwable {
             MethodType mt=MethodType.methodType(void.class);
+            /*java8之后，findSpecial做了调整，它只会从第4个参数的直接父类里面找方法，这点
+            * 和书上不一致。*/
             MethodHandle mh=lookup().findSpecial(GrandFather.class,"thinking",mt,getClass());
             mh.invoke(this);
         }
