3caa8b3bebbaecaadd00a2a8706940eee9c3622a
HymanLiuTS <879651072@qq.com>
2021-03-18 19:49:43 +0800

    命名检查器

--- a13complementts/src/namecheck/NameCheckProcessor.java
+++ a13complementts/src/namecheck/NameCheckProcessor.java
@@ -1,10 +1,8 @@
 package namecheck;
 
-import javax.annotation.processing.AbstractProcessor;
-import javax.annotation.processing.RoundEnvironment;
-import javax.annotation.processing.SupportedAnnotationTypes;
-import javax.annotation.processing.SupportedSourceVersion;
+import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
 import java.util.Set;
 
@@ -19,8 +17,22 @@ import java.util.Set;
 @SupportedAnnotationTypes("*")
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
 public class NameCheckProcessor extends AbstractProcessor {
+
+    private NameChecker nameChecker;
+
+    @Override
+    public synchronized void init(ProcessingEnvironment processingEnv) {
+        super.init(processingEnv);
+        nameChecker = new NameChecker(processingEnv);
+    }
+
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        if (!roundEnv.processingOver()) {
+            for (Element element : roundEnv.getRootElements()) {
+                nameChecker.checkName(element);
+            }
+        }
         return false;
     }
 }
--- a13complementts/src/namecheck/NameChecker.java
+++ a13complementts/src/namecheck/NameChecker.java
@@ -2,9 +2,10 @@ package namecheck;
 
 import javax.annotation.processing.Messager;
 import javax.annotation.processing.ProcessingEnvironment;
-import javax.lang.model.element.Element;
+import javax.lang.model.element.*;
 import javax.lang.model.util.ElementScanner8;
 import javax.tools.Diagnostic;
+import java.util.EnumSet;
 
 /**
  * @author ：Hyman
@@ -14,14 +15,85 @@ import javax.tools.Diagnostic;
  * @version: $
  */
 public class NameChecker {
+
     private final Messager messager;
+
+    NameCheckScanner nameCheckScanner = new NameCheckScanner();
 
     public NameChecker(ProcessingEnvironment processingEnvironment) {
         this.messager = processingEnvironment.getMessager();
     }
 
+    public void checkName(Element element) {
+        nameCheckScanner.scan(element);
+    }
+
     public class NameCheckScanner extends ElementScanner8 {
 
+        /**
+         * create by: Hyman
+         * description: 检查java类是否合法
+         * create time: 2021/3/18
+         */
+        public Void visitType(TypeElement e, Void p) {
+            scan(e.getEnclosedElements(), p);
+            checkCamelCase(e, true);
+            super.visitType(e, p);
+            return null;
+        }
+
+        /**
+         * create by: Hyman
+         * description: 检查方法命名是否合法
+         * create time: 2021/3/18
+         */
+        public Void visitVariable(VariableElement e, Void p) {
+            if (e.getKind() == ElementKind.METHOD) {
+                Name name = e.getSimpleName();
+                if (name.contentEquals(e.getEnclosingElement().getSimpleName())) {
+                    messager.printMessage(Diagnostic.Kind.WARNING, "一个普通方法：" + name + "，不应当与类名重复", e);
+                    checkCamelCase(e, false);
+                }
+            }
+            super.visitVariable(e, p);
+            return null;
+        }
+
+        /**
+         * create by: Hyman
+         * description: 检查变量命名是否合法
+         * create time: 2021/3/18
+         */
+        public Void visitExecutable(VariableElement e, Void p) {
+            if (e.getKind() == ElementKind.ENUM_CONSTANT ||
+                    e.getConstantValue() != null) {
+                checkAllCaps(e);
+            } else {
+                checkCamelCase(e, false);
+            }
+            return null;
+        }
+
+        /**
+         * create by: Hyman
+         * description: 判断一个变量是不是常量
+         * create time: 2021/3/18
+         */
+        private boolean heuristcallyConstant(VariableElement e) {
+            if (e.getEnclosingElement().getKind() == ElementKind.INTERFACE) {
+                return true;
+            } else if (e.getKind() == ElementKind.FIELD &&
+                    e.getModifiers().containsAll(EnumSet.of(Modifier.PUBLIC, Modifier.STATIC, Modifier.FINAL))) {
+                return true;
+            }
+            return false;
+        }
+
+        /**
+         * create by: Hyman
+         * description: 检查传入的Element是否符合驼峰命名
+         * create time: 2021/3/18
+         */
         private void checkCamelCase(Element e, boolean initialCaps) {
             String name = e.getSimpleName().toString();
             boolean previousUpper = false;
@@ -46,12 +118,14 @@ public class NameChecker {
 
         /**
          * create by: Hyman
-         * description:
+         * description: 大写命名检查，要求第一个字母必须是大写字母
          * create time: 2021/3/17
          */
         private void checkAllCaps(Element e) {
             String name = e.getSimpleName().toString();
             //todo
         }
+
+
     }
 }
