ce77861ecdb0ab055d7a6ebe705802e8bdc61d02
HymanLiuTS <879651072@qq.com>
2021-04-01 12:23:40 +0800

    反射

---
+++ a08ReflectionTS/src/Test.java
@@ -0,0 +1,19 @@
+/**
+ * @author ：Hyman
+ * @date ：Created in 2021/4/1 11:36
+ * @description：
+ * @modified By：
+ * @version: $
+ */
+public class Test {
+    private String name;
+    private int age;
+
+    void Test() {
+    }
+
+    void Test(String name, int age) {
+        this.age = age;
+        this.name = name;
+    }
+}
